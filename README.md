38ddad5b4e5442bcc5eea575df071fdf4180e5c4
Marcos Bernardes <zvsghhrdys@privaterelay.appleid.com>
2026-09-20 21:21:04 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto inicial - Criando um repositório no GitHub
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Novos Deploys</h1>
+  return <h1>Novos Deploys</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
