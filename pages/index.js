0c115c092f5440bd6d6b95c0c47f142fbc552888
Marcos Bernardes <zvsghhrdys@privaterelay.appleid.com>
2026-09-20 21:37:33 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Novos Deploys</h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("identação errada");
-}
-
 export default Home;
